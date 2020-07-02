fb5bb68a205c1ad2fff48aed23770b6c73eeff88
uni51 <uni51@users.noreply.github.com>
2020-07-02 16:33:53 +0900

    2-4 リファクタをしよう

---
+++ front/src/services/calendar.js
@@ -0,0 +1,19 @@
+import dayjs from "dayjs";
+import "dayjs/locale/ja";
+dayjs.locale("ja");
+
+export const createCalendar = () => {
+  // 今月の最初の日を追加
+  const firstDay = dayjs().startOf("month");
+
+  const firstDayIndex = firstDay.day();
+
+  return Array(35)
+    .fill(0)
+    .map((_, i) => {
+      const diffFromFirstDay = i - firstDayIndex;
+      const day = firstDay.add(diffFromFirstDay, "day");
+
+      return day;
+    });
+};
